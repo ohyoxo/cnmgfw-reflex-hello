f2712a2334055779c2e57dc6818f7022ef78aabb
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2025-03-07 18:32:43 +0800

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,10 +1,9 @@
-# rxconfig.py
 import reflex as rx
 
-# 配置应用
 config = rx.Config(
     app_name="hello",
     api_url="http://localhost:8000",
-    frontend_port=3000,
+    db_url="sqlite:///reflex.db",
+    frontend_port=3000,  # 与原始项目 PORT 对应
     backend_port=8000,
 )
