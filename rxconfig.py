6e022618644b22c4ef3d6e71798e091d1d83cf05
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2025-03-07 22:30:47 +0800

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,8 @@
+# rxconfig.py
 import reflex as rx
 
 config = rx.Config(
-    app_name="hello",
-    api_url="http://localhost:8000",
-    db_url="sqlite:///reflex.db",
-    frontend_port=3000,  # 与原始项目 PORT 对应
+    app_name="cnmgfw-reflex-hello",
+    frontend_port=3000,
     backend_port=8000,
 )
