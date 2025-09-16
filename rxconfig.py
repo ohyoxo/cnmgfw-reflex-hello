aa87d360ba8953a869c012b5355f8676da334bca
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2025-09-17 00:15:19 +0800

    Add files via upload

--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,11 @@
 import reflex as rx
+from my_app.api import app as api_app
 
 config = rx.Config(
     app_name="my_app",
     db_url="sqlite:///reflex.db",
     api_url="http://0.0.0.0:8000",
     disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
+    app_module="my_app.my_app",
+    api_module="my_app.api:app",
 )
