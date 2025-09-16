d71082587084cb4b5f2b485c27ab46edae3fe487
ohyoxo <126952400+ohyoxo@users.noreply.github.com>
2025-09-16 22:21:52 +0800

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,8 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="your-app-name",  # <-- 在这里修改你的应用名！
+    app_name="myapps",  # <-- 在这里修改你的应用名！
     # 部署后的网址将是 your-app-name.reflex.run
     disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
 )
+
